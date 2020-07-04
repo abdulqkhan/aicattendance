f24e7241da0a37ca5cb34da33e182c0036a3f530
abdulqkhan <abdulkhan.email@gmail.com>
2020-07-04 19:15:26 +1000

    Update my.js

--- my.js
+++ my.js
@@ -1,5 +1,29 @@
 
 
+
+function addGS() {
+
+    const url = "https://script.google.com/macros/s/AKfycbyO1nYaT1eZQyAfzPVll2b8BYAx0RCHDQGRhOsX7HJcqy8lAEm-/exec";
+
+    // fetch needs to send bookingID.
+    fetch(url, {
+
+        method: 'POST',
+        mode: 'no-cors',
+        cache: 'no-cache',
+        credentials: 'omit',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        redirect: 'follow',
+        referrerPolicy: 'no-referrer',
+        body: JSON.stringify({bookingID:"test booking id"})
+
+    });
+
+}
+
+
 function testGS() {
 
     const url = "https://script.google.com/macros/s/AKfycbyO1nYaT1eZQyAfzPVll2b8BYAx0RCHDQGRhOsX7HJcqy8lAEm-/exec";
@@ -16,5 +40,5 @@ function testGS() {
     });
 
 }
-
+document.getElementById("btn2").addEventListener("click", addGS);
 document.getElementById("btn").addEventListener("click", testGS);
